26394cf879c6ce66020e4d61a38f51b67772f88e
thejoltjoker <hello@thejoltjoker.com>
2017-09-27 21:09:05 -0700

    again

--- sandbox/list_attributes.py
+++ sandbox/list_attributes.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 """
 list_attributes.py
-<<<<<<< HEAD
 Lists all attributes and their respective values, for selected object. Disregards attribute types: message, compound.
 """
 import maya.cmds as cmds
-=======
-Description of list_attributes.py.
-"""
->>>>>>> dd39bd6b508171453c9dc36e90b2d4418283fbb7
 
 
 def main():
     """docstring for main"""
     all_nodes = cmds.ls(sl=True)
-<<<<<<< HEAD
     for node in all_nodes:
         print("Attributes for {}".format(node))
         attribute_list = cmds.listAttr(node)
@@ -28,20 +22,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-
-    for node in all_nodes:
-        print("Attributes for {}".format(node))
-        attribute_list = cmds.listAttr(node)
-
-        for attr in attribute_list:
-
-            if attr != 'message':
-                attribute_value = cmds.getAttr('{}.{}'.format(node, attr))
-
-                print("{} = {}".format(attr, attribute_value))
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> dd39bd6b508171453c9dc36e90b2d4418283fbb7
