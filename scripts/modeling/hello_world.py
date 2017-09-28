db45e0e6c506fbb1ebb6c46e7679ffa195bafdf7
thejoltjoker <hello@thejoltjoker.com>
2017-09-27 22:20:09 -0700

    add inits

---
+++ scripts/modeling/hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+hello_world.py
+Description of hello_world.py.
+"""
+import maya.cmds as cmds
+
+
+def main():
+    """docstring for main"""
+    cmds.warning("hello world")
+
+
+if __name__ == '__main__':
+    main()
