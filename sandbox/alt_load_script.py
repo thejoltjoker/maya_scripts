ee37946a2e7faa03a93148a77bce8762a8e3d243
Johannes Andersson <hello@thejoltjoker.com>
2021-12-10 21:13:03 +0100

    delete scripts

--- sandbox/alt_load_script.py
+++
@@ -1,4 +0,0 @@
-import imp
-
-foo = imp.load_source('maya', 'D:/people/johannes/scripts/repos/sequence/maya/menu.py')
-maya.theSequenceMenu()
--- scripts/texturing/print_all_texture_file_paths.py
+++
@@ -1,7 +0,0 @@
-allTextures = cmds.ls(tex=True)
-
-for texture in allTextures:
-    if cmds.nodeType(texture) == 'file':
-        texturePath = cmds.getAttr(texture + ".fileTextureName")
-
-        print texture + " = " + texturePath
