4ae584270d5ed45834e8503c4a99105d2a875a57
Johannes Andersson <hello@thejoltjoker.com>
2022-05-19 10:58:35 +0200

    update paths

--- scripts/files/dirmap.py
+++ scripts/files/dirmap.py
@@ -1,8 +1,18 @@
+import sys
+
 from maya import cmds
 
 
 def main():
-    cmds.dirmap(m=('P:/', '/Volumes/macpath/'))
+    win_path = 'L:/'
+    mac_path = '/Users/johannes/frank/libraries/'
+    cmds.dirmap(en=True)
+    if sys.platform == 'darwin':
+        cmds.dirmap(m=(win_path, mac_path))
+    else:
+        cmds.dirmap(m=(mac_path, win_path))
+    print(win_path, '=', cmds.dirmap(cd=win_path))
+    print(mac_path, '=', cmds.dirmap(cd=mac_path))
 
 
 if __name__ == '__main__':
