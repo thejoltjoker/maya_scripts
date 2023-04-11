4fa5fbb93ca0b35be8e0c70258ef614d14d4d861
Johannes <hello@thejoltjoker.com>
2023-04-11 20:27:14 +0200

    update pastebin ui

--- scripts/workflow/pastebin.py
+++ scripts/workflow/pastebin.py
@@ -13,6 +13,8 @@ import tempfile
 import maya.cmds as cmds
 import maya.OpenMaya as om
 from sys import platform as system_os
+from maya import OpenMayaUI as omui
+from shiboken2 import wrapInstance
 
 PRINT_PREFIX = '[pastebin] '
 CACHE_FOLDER_NAME = 'pastebin'
@@ -127,27 +129,37 @@ def open_folder(*args, **kwargs):
 
 import maya.cmds as cmds
 from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtWidgets import QSizePolicy
 
 
-class PastebinWindow(QtWidgets.QDialog):
+class PastebinWindow(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(PastebinWindow, self).__init__(parent)
+        # self.setParent(mayaMainWindow)
 
         # Set window properties
         self.setWindowTitle('Pastebin')
-        # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowTitleHint)
-        self.setMinimumSize(250, 100)
+        self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint)
+        self.setMinimumSize(250, 200)
+
+        font = QtGui.QFont("Jetbrains Mono")
+        font.setStyleHint(QtGui.QFont.Monospace)
+        self.setFont(font)
 
         # Create button widgets
         self.label_path = QtWidgets.QLabel(CACHE_FOLDER_PATH)
         self.label_path.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
-        self.label_path.setStyleSheet('color: #48cae4; text-decoration: underline')
-        # self.label_path.setOpenExternalLinks(False)
-        # self.label_path.linkActivated.connect(open_folder)
+        self.label_path.setStyleSheet(
+            'color: #48cae4;'
+            'text-decoration: underline;'
+            'cursor: pointer;'
+            'font-family: Jetbrains Mono, monospace;')
         self.label_path.mousePressEvent = open_folder
 
         self.btn_copy = QtWidgets.QPushButton('Copy')
+        self.btn_copy.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.btn_paste = QtWidgets.QPushButton('Paste')
+        self.btn_paste.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.btn_open = QtWidgets.QPushButton('Open cache folder')
 
         # Create button layout
@@ -170,9 +182,11 @@ class PastebinWindow(QtWidgets.QDialog):
 
 
 def main():
-    # Create and show the popup dialog
-    popup = PastebinWindow()
-    popup.exec_()
+    mayaMainWindowPtr = omui.MQtUtil.mainWindow()
+    mayaMainWindow = wrapInstance(int(mayaMainWindowPtr), QtWidgets.QWidget)
+    ui = PastebinWindow(mayaMainWindow)
+    ui.show()
+    return ui
 
 
 if __name__ == '__main__':
